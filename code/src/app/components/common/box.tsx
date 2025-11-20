d27926cfabb93cabe7742721569e3953fe1da1d8
prachinayak1509 <prachinayak1509@gmail.com>
2025-11-20 14:37:48 +0530

    mobile & desktop margin

--- code/src/app/components/common/box.tsx
+++ code/src/app/components/common/box.tsx
@@ -18,52 +18,75 @@ export default function Box({ image,
         rounded-[30px] 
         bg-[linear-gradient(to_bottom,#F2E3E1_0%,#DBC2BD_100%)]
         shadow-[0_6px_20px_0_rgba(0,0,0,0.4)]
-        p-6 flex flex-col items-center text-center
+        md:p-6 p-4
+        flex flex-col md:flex-col 
+        md:items-center 
+        md:text-center
+        text-left
         my-3
-        md:w-[450px]
+        md:w-[600px]
         md:h-[530px]
-        h-[200px]
         ${className || ""}
       `}
     >
-      {/* <div className=" flex flex-col items-center"> */}
-      {/* ICON */}
-      <div className="mb-4">
+
+      {/* MOBILE LAYOUT */}
+      <div className="flex md:hidden flex-row items-center gap-4">
+        <Image
+          src={image}
+          alt={title}
+          width={80}
+          height={80}
+          className="object-contain"
+        />
+
+        <div className="flex flex-col justify-center">
+          <h3 className="font-semibold text-expresso">{title}</h3>
+          <p className="text-liver-brown text-sm">{subtitle}</p>
+
+          
+          {buttonText && (
+            <div className="mt-4">
+              <Button onClick={onButtonClick} text={buttonText} />
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* DESKTOP LAYOUT */}
+      <div className="hidden md:flex flex-col items-center text-center flex-grow">
+
+        {/* ICON */}
         <Image
           src={image}
           alt={title}
           width={100}
           height={100}
-          className="mx-auto object-contain"
+          className="mb-4 object-contain"
         />
-      </div>
-      <div className="">
+
         {/* TITLE */}
-        <h3 className="font-semibold mt-2 text-expresso">
-          {title}
-        </h3>
+        <h3 className="font-semibold mt-2 text-expresso">{title}</h3>
 
         {/* SUBTITLE */}
-        <p className="text-liver-brown mt-1 text-center">
-          {subtitle}
-        </p>
+        <p className="text-liver-brown mt-1">{subtitle}</p>
 
-        {/* DESCRIPTION */}
+        {/* DESCRIPTION (only desktop) */}
         {description && (
           <p className="text-liver-brown mt-15 hidden md:block">
             {description}
           </p>
         )}
-        {children}
 
-        {/* BUTTON */}
-        {buttonText && (
-          <div className="mt-6 absolute bottom-15 left-0 right-0">
-            <Button onClick={onButtonClick} text={buttonText} />
-          </div>
-        )}
+        {children}
       </div>
+
+      {/* DESKTOP BUTTON*/}
+      {buttonText && (
+        <div className="hidden md:flex absolute mt-6 bottom-10 left-0 right-0 justify-center">
+          <Button onClick={onButtonClick} text={buttonText} />
+        </div>
+      )}
     </div>
-    // </div>
   );
 }
--- code/src/app/portfolio/portfolio.tsx
+++ code/src/app/portfolio/portfolio.tsx
@@ -2,10 +2,16 @@ import Box from "../components/common/box";
 
 export default function Portfolio() {
   return (
-    <div className="mx-5 my-10">
+    // <div className="mx-4 md:mx-15 my-6 md:my-10 ">
+    <div className=" mx-auto
+    w-full
+    px-4 md:px-20 lg:px-15
+    
+    py-6 md:py-10">
       <h2>Building Smart Tools for a Smarter World</h2>
       <h3> Explore our protfolio of AI, apps and web innovation.</h3>
-      <div className="flex flex-col md:flex-row gap-6 justify-between">
+      <div className="flex flex-col md:flex-row justify-between gap-[20%] md:gap-4">
+        {/* <div className="flex flex-col md:flex-row justify-between gap-8"> */}
         <Box
           image="/sales.svg"
           title="AI Agent"
@@ -13,7 +19,7 @@ export default function Portfolio() {
           description="Stop losing leads to late replies. Implement the SmallTech AI Sales Agent and automate 80% of your sales pipeline. "
           buttonText="Try Demo"
           onButtonClick={() => console.log("Clicked!")}
-          className="max-w-[420px]"
+          className="w-full max-w-[400px]"
         />
         <Box
           image="/app.svg"
@@ -22,7 +28,7 @@ export default function Portfolio() {
           description="It is the fastest way to report local potholes, illegal dumping, broken lights, and other civic issues directly to your local government. "
           buttonText="Download"
           onButtonClick={() => console.log("Clicked!")}
-          className="max-w-[420px]"
+          className="w-full max-w-[400px]"
         />
         <Box
           image="/blog.svg"
@@ -31,7 +37,7 @@ export default function Portfolio() {
           description="Stop staring at a blank screen. Our AI Blog Generator turns your ideas into engaging posts."
           buttonText="Visit AI Blog"
           onButtonClick={() => console.log("Clicked!")}
-          className="max-w-[420px]"
+          className="w-full max-w-[400px]"
         />
       </div>
     </div>
