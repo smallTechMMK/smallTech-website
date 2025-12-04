c503b7aa0dd1b43dc935e5019af62bd0eac2a89a
prachinayak1509 <prachinayak1509@gmail.com>
2025-12-04 13:57:29 +0530

    footer size

--- code/src/app/domains/domains.tsx
+++ code/src/app/domains/domains.tsx
@@ -33,7 +33,7 @@ export default function Domains() {
       </div>
 
       {/* MOBILE SCREEN 2: BUSINESS DOMAINS */}
-      <div className="md:hidden min-h-screen flex flex-col justify-start py-6">
+      <div className="md:hidden min-h-screen flex flex-col justify-start py-6 ">
         <section className="w-full">
 
           <h2 className="mb-2">Business Workflows</h2>
@@ -55,8 +55,8 @@ export default function Domains() {
       </div>
 
       {/* DESKTOP VIEW*/}
-      <div className="hidden md:flex flex-col">
-
+      <div className="hidden md:flex flex-col min-h-screen">
+<div className=" flex-grow">
         {/* DESKTOP: TECH DOMAINS */}
         <h2 className="">Tech Domains</h2>
         <h3 className="mb-5 font-[200]">
@@ -93,7 +93,10 @@ export default function Domains() {
           ))}
         </div>
       </div>
+     
       <Footer className= "hidden md:block " />
+      
+</div>
     </div>
   );
 }
